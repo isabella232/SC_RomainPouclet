0002c23d2ddd47af71b547ceb19ed6ca563d691d
Romain Pouclet <romain.pouclet@gmail.com>
2017-12-03 17:19:25 -0500

    Initial Commit

---
+++ RW-SwiftGen-GithubIssues/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RW-SwiftGen-GithubIssues
+//
+//  Created by Romain Pouclet on 2017-12-03.
+//  Copyright © 2017 Perfectly-Cooked. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
