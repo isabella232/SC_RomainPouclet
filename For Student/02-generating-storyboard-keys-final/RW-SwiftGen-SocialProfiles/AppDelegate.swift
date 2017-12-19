72e1ab2f740dd22fd67ff18446ccb6959de5a997
Romain Pouclet <romain.pouclet@gmail.com>
2017-12-19 15:41:12 -0500

    Use swiftgen to generate typesafe stuff for storyboards

--- For Student/02-generating-storyboard-keys-final/RW-SwiftGen-SocialProfiles/AppDelegate.swift
+++ For Student/02-generating-storyboard-keys-final/RW-SwiftGen-SocialProfiles/AppDelegate.swift
@@ -33,4 +33,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        // Creates the profiles view controller
+        let profiles = StoryboardScene.Main.profiles.instantiate()
+
+        let window = UIWindow()
+        window.rootViewController = UINavigationController(rootViewController: profiles)
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+
 }
