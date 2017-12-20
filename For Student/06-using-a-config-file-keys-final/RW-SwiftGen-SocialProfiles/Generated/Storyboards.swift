0897fd0887f5267eab6d309d64a1376ae1c12b36
Romain Pouclet <romain.pouclet@gmail.com>
2017-12-19 19:42:10 -0500

    Use config file

---
+++ For Student/06-using-a-config-file-keys-final/RW-SwiftGen-SocialProfiles/Generated/Storyboards.swift
@@ -0,0 +1,73 @@
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+
+// swiftlint:disable sorted_imports
+import Foundation
+import UIKit
+import RW_SwiftGen_SocialProfiles
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+protocol StoryboardType {
+  static var storyboardName: String { get }
+}
+
+extension StoryboardType {
+  static var storyboard: UIStoryboard {
+    let name = self.storyboardName
+    return UIStoryboard(name: name, bundle: Bundle(for: BundleToken.self))
+  }
+}
+
+struct SceneType<T: Any> {
+  let storyboard: StoryboardType.Type
+  let identifier: String
+
+  func instantiate() -> T {
+    let identifier = self.identifier
+    guard let controller = storyboard.storyboard.instantiateViewController(withIdentifier: identifier) as? T else {
+      fatalError("ViewController '\(identifier)' is not of the expected class \(T.self).")
+    }
+    return controller
+  }
+}
+
+struct InitialSceneType<T: Any> {
+  let storyboard: StoryboardType.Type
+
+  func instantiate() -> T {
+    guard let controller = storyboard.storyboard.instantiateInitialViewController() as? T else {
+      fatalError("ViewController is not of the expected class \(T.self).")
+    }
+    return controller
+  }
+}
+
+protocol SegueType: RawRepresentable { }
+
+extension UIViewController {
+  func perform<S: SegueType>(segue: S, sender: Any? = nil) where S.RawValue == String {
+    let identifier = segue.rawValue
+    performSegue(withIdentifier: identifier, sender: sender)
+  }
+}
+
+// swiftlint:disable explicit_type_interface identifier_name line_length type_body_length type_name
+enum StoryboardScene {
+  enum Main: StoryboardType {
+    static let storyboardName = "Main"
+
+    static let profile = SceneType<RW_SwiftGen_SocialProfiles.ProfileViewController>(storyboard: Main.self, identifier: "Profile")
+
+    static let profiles = SceneType<RW_SwiftGen_SocialProfiles.ProfilesViewController>(storyboard: Main.self, identifier: "Profiles")
+  }
+}
+
+enum StoryboardSegue {
+  enum Main: String, SegueType {
+    case profileDetail = "ProfileDetail"
+  }
+}
+// swiftlint:enable explicit_type_interface identifier_name line_length type_body_length type_name
+
+private final class BundleToken {}
